ec6d0acf94ba474022d7acf461bdaaecdbac20e9
Maarten Van Hoof <maartenvanhoof1989@gmail.com>
2019-05-04 23:24:00 +0200

    feat(Netlify CMS): Add identity

---
+++ gridsome.client.js
@@ -0,0 +1,39 @@
+/**
+ * Inspired by gatsby-plugin-netlify-cms
+ * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-plugin-netlify-cms/src/gatsby-browser.js
+ */
+
+// Taken from https://github.com/netlify/netlify-identity-widget
+const routes = /(confirmation|invite|recovery|email_change)_token=([^&]+)/
+const errorRoute = /error=access_denied&error_description=403/
+const accessTokenRoute = /access_token=/
+
+const onInitialClientRender = ({ hash, enableIdentityWidget, publicPath }) => {
+  if (
+    enableIdentityWidget &&
+    (routes.test(hash) || errorRoute.test(hash) || accessTokenRoute.test(hash))
+  ) {
+    import(`netlify-identity-widget`).then(
+      ({ default: netlifyIdentityWidget }) => {
+        netlifyIdentityWidget.on(`init`, user => {
+          if (!user) {
+            netlifyIdentityWidget.on(`login`, () => {
+              document.location.href = `${publicPath}/`
+            })
+          }
+        })
+        netlifyIdentityWidget.init()
+      }
+    )
+  }
+}
+
+export default function(Vue, { enableIdentityWidget, publicPath }, { router }) {
+  router.onReady(({ hash }) =>
+    onInitialClientRender({
+      enableIdentityWidget,
+      publicPath,
+      hash,
+    })
+  )
+}
